fd08123362b927a4d5f8ff3049e7ce63d89f1566
Mike O'Connor <mike@oeg.com.au>
2022-04-11 16:25:26 +0930

    Added scripts directory to klipper config

---
+++ scripts/generate_graphs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILES=`ls /tmp/calibration_data_*.csv`
+
+mkdir -p ~/klipper_config/graphs
+
+for file in ${FILES}
+do
+	name=`basename ${file} .csv`
+	~/klipper/scripts/calibrate_shaper.py $file ~/klipper_config/graphs/${name}.png
+	mv $file $file.processed 
+done
+
