c2cddc9a8e7e8c44ce109a9896e39d10f9aedfac
Mike O'Connor <mike@pineview.net>
2022-05-30 13:58:24 +0930

    enabled the mv in generate graphs

--- scripts/generate_graphs.sh
+++ scripts/generate_graphs.sh
@@ -8,6 +8,6 @@ for file in ${FILES}
 do
 	name=`basename ${file} .csv`
 	~/klipper/scripts/calibrate_shaper.py $file -o ~/klipper_config/graphs/${name}.png
-	# mv $file $file.processed 
+	mv $file $file.processed 
 done
 
